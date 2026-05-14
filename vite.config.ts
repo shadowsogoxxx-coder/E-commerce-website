9b4766f33ae9282710be383d39cda562777755d6
shadowsogoxxx-coder <shadowsogoxxx@gmail.com>
2026-05-15 02:49:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => ({
   },
   // Add this build section below
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
